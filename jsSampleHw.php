88c3f903b83c7dc8046a980c1a6711273cb47aa3
Vanshika B Patel <vbp39@afsconnect1.njit.edu>
2019-10-02 18:49:33 -0400

    jsSample homework

--- jsSampleHw.php
+++ jsSampleHw.php
@@ -1,18 +1,19 @@
 <html>
 <head>
 	<script>
+		//TODO solutions
+                //Google/Explore how to create an element and add it to the DOM
+                //create a div tag, add "added new element" as the text
+                //add it to the DOM body
+
+
 	function newElement(){
 		var newElement = document.createElement ('div');
 		newElement.textContent = 'added a new element';
 		console.log(newElement);
 
 		document.body.append(newElement);
-
-		 //TODO solutions
-		//Google/Explore how to create an element and add it to the DOM
-		//create a div tag, add "added new element" as the text
-		//add it to the DOM body
-	}
+}
 
 	</script>
 </head>
